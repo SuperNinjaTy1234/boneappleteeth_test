add43118c599d7d140202a21321d9d9fe4c4b113
jta00007 <jta00007@mix.wvu.edu>
2024-03-25 16:32:29 -0400

    Changed main page to login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+/*import './App.css';
 import './Pages/ProfilePage/ProfilePopup';
 import './Pages/ProfilePage/ProfilePage'
 import { BrowserRouter, Routes, Route, useHref} from "react-router-dom";
@@ -24,4 +24,23 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+export function App({ signOut, user }: WithAuthenticatorProps) {
+  return (
+    <>
+      <h1>Hello {user?.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
+
