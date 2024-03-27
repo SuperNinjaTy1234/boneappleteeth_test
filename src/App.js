524b39779cb4bf67d90215ff1c08c5a563bf85c9
jta00007 <jta00007@mix.wvu.edu>
2024-03-27 14:58:35 -0400

    Updated Login to work

--- src/App.js
+++ src/App.js
@@ -36,8 +36,6 @@ Amplify.configure(config);
 export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
     <>
-    <h1>Hello {user?.username}</h1>
-      <button onClick={signOut}>Sign out</button>
       <div className="App">
       <BrowserRouter>
         <Routes>
--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -6,7 +6,14 @@ import ProfileStyle from './ProfilePage.module.css';
 import RecipeCard from './ProfileRecipeCard';
 import FavoritesCard from './ProfileFavoritesCard';
 
-const UserProfile = () => {
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from '../../amplifyconfiguration.json';
+Amplify.configure(config); 
+
+const UserProfile = ({ signOut, user }: WithAuthenticatorProps) => {
     return (
         <>
           <Navbar></Navbar>
@@ -15,7 +22,7 @@ const UserProfile = () => {
                     <img src="https://static.wikia.nocookie.net/412c91cd-50bd-4c0c-8332-6cd5a1b2ec98/scale-to-width/755" alt="Profile Picture" />
                     {/* Just an img for fun but we can use the blank profile img i uploaded for the presentation. */}
                 </div>
-                <div className={ProfileStyle.username}>Username(Me after coding)</div>
+                <div className={ProfileStyle.username}>Welcome {user?.username}!</div>
                 <div className={ProfileStyle.section_container}>
                     <div className={ProfileStyle.section}>
                         <h2>My Recipes</h2>
@@ -30,9 +37,10 @@ const UserProfile = () => {
                         </ul>
                     </div>
                 </div>
+                <button className={ProfileStyle.logout_btn} onClick={signOut}>Sign out</button>
             </div>
         </>
     );
 };
 
-export default UserProfile;
+export default withAuthenticator(UserProfile);
